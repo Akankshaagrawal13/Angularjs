c74c8db0502be489c24cc7ddbdd579b74e4b49f9
akanksha13 <agrawal.akanksha137@gmail.com>
2018-02-19 21:37:25 +0530

    Basic Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgModule } from '@angular/core';
+import { DishService } from './services/dish.service';
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
@@ -22,7 +23,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MaterialModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
