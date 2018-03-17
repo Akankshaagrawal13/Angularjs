7a16b741b05e60cc00305a7e58a25954a887ec05
akanksha13 <agrawal.akanksha137@gmail.com>
2018-03-17 18:58:35 +0530

    Leader service setup

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,15 +1,44 @@
 import { Injectable } from '@angular/core';
 import { Leader } from '../shared/leader';
-import { LEADERS } from '../shared/leaders';
+import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { BaseURL } from '../shared/baseurl';
+import { ProcessHTTPMsgService } from './process-httpmsg.service';
+import { RestangularModule, Restangular } from 'ngx-restangular';
 import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/catch';
+
+//import { LEADERS } from '../shared/leaders';
 
 @Injectable()
 export class LeaderService {
 
-  constructor() { }
-   getLeaders(): Observable<Leader[]> {
+  constructor(private restangular: Restangular,
+              private processHTTPMsgService: ProcessHTTPMsgService) { }
+
+ getLeaders(): Observable<Leader[]> {
+    return this.restangular.all('leaders').getList();
+  }
+
+  getLeader(id: number): Observable<Leader> {
+    return  this.restangular.one('leaders',id).get();
+  }
+
+  getFeaturedLeader(): Observable<Leader> {
+    return this.restangular.all('leaders').getList({featured: true})
+      .map(leaders => leaders[0]);
+  }
+
+  getLeaderIds(): Observable<number[]> {
+    return this.getLeaders()
+      .map(leaders => { return leaders.map(leader => leader.id) })
+      .catch(error => { return error; } );
+  }
+
+
+ getLeaders(): Observable<Leader[]> {
     return Observable.of(LEADERS).delay(2000);
   }
 
